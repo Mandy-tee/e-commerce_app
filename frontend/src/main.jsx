95e8383691523dbe9f9e573595657296a9854f6b
Amanda Tagoe <nayork77@gmail.com>
2025-01-05 10:55:54 +0000

    Changes made in the code structure of the main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ShopContextProvider from './context/ShopContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <ShopContextProvider>
-    <App />
-  </ShopContextProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
+  </React.StrictMode>
 );
