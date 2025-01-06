999f9b285ee4afcf933ba450f3fad229fdf17035
Amanda Tagoe <nayork77@gmail.com>
2025-01-06 20:47:03 +0000

    Changes made in backend and admin folders

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
-const { type } = require('os');
-const { log } = require('console');
+ 
 
 app.use(express.json());
 app.use(cors());
@@ -112,6 +111,7 @@ app.post('/addproduct', async (req, res) => {
 // Creating API for Deleting Products
 
 app.post('/removeproduct', async (req, res) => {
+    console.log(req.body);
     await Product.findOneAndDelete({ id: req.body.id });
     console.log('Removed');
     res.json({
@@ -127,6 +127,61 @@ app.get('/allproducts', async (req, res) => {
     console.log('All Products Fetched');
     res.send(products);
 })
+
+// Schema creating for User model
+
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+// Creating Endpoint for registering the User
+app.post('/signup', async (req, res) => {
+
+    let check = await Users.findOne({email:req.body.email});
+    if (check) {
+        return res.status(400).json({success:false, errors:'existing user found with same email address'})
+    }
+    let cart = {}
+    for (let i = 0; i < 300; i++) {
+        cart[i]=0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({success:true,token})
+})
+
+// Creating endpoint for user login
+
+
 
 app.listen(port, (error) => {
     if (!error) {
